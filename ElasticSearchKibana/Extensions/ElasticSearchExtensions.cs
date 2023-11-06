b278b326e5c82ff48f795f652062ae6002cdf3b1
Muhammadjon Mahkamboyev <muhammadjonmakhkamov1@gmail.com>
2023-11-06 10:49:58 +0500

    Changes made

--- ElasticSearchKibana/Extensions/ElasticSearchExtensions.cs
+++ ElasticSearchKibana/Extensions/ElasticSearchExtensions.cs
@@ -1,5 +1,29 @@
-﻿namespace ElasticSearchKibana.Extensions;
+﻿using ElasticSearchKibana.Entities;
+using Nest;
+
+namespace ElasticSearchKibana.Extensions;
 
 public static class ElasticSearchExtensions
 {
-}
+    public static void AddElasticSearch(this IServiceCollection services, IConfiguration configuration)
+    {
+        var url = configuration["ELKConfiguration:Url"];
+        var defaultIndex = configuration["ELKConfiguration:index"];
+
+        var settings = new ConnectionSettings(new Uri(url!))
+            .PrettyJson().DefaultIndex(defaultIndex);
+        AddDefaultMapping(settings: settings);
+        var client = new ElasticClient(settings);
+        services.AddSingleton<IElasticClient>(client);
+        
+
+    }
+
+    private static void AddDefaultMapping(ConnectionSettings settings)
+    {
+        settings.DefaultMappingFor<Product>(
+            p => p
+                .Ignore(p => p.Id));
+    }
+    
+}   
