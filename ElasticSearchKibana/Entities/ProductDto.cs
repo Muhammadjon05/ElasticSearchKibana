c295ea2342d4aa3050afbd928a1571bca40e0787
Muhammadjon Mahkamboyev <muhammadjonmakhkamov1@gmail.com>
2023-11-06 11:03:47 +0500

    elasticsearch added

---
+++ ElasticSearchKibana/Entities/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ElasticSearchKibana.Entities;
+
+public class ProductDto
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
--- ElasticSearchKibana/Extensions/ElasticSearchExtensions.cs
+++ ElasticSearchKibana/Extensions/ElasticSearchExtensions.cs
@@ -9,15 +9,18 @@ public static class ElasticSearchExtensions
     {
         var url = configuration["ELKConfiguration:Url"];
         var defaultIndex = configuration["ELKConfiguration:index"];
-
         var settings = new ConnectionSettings(new Uri(url!))
             .PrettyJson().DefaultIndex(defaultIndex);
         AddDefaultMapping(settings: settings);
         var client = new ElasticClient(settings);
         services.AddSingleton<IElasticClient>(client);
-        
-
+        CreateIndex(client,defaultIndex!);
     }
+    /// <summary>
+    /// Below dagi code bizga map qilishda fieldlarini include qilish yoki
+    /// qilmasilk uchun yordam beradi
+    /// </summary>
+    /// <param name="settings"></param>
 
     private static void AddDefaultMapping(ConnectionSettings settings)
     {
@@ -25,5 +28,10 @@ public static class ElasticSearchExtensions
             p => p
                 .Ignore(p => p.Id));
     }
-    
+
+    private static void CreateIndex(IElasticClient client, string indexName)
+    {
+        client.Indices.Create(indexName.ToLower(), i => i.
+            Map<Product>(x => x.AutoMap()));
+    }
 }   
